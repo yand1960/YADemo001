f591502bb6d07396e50131174d7f09257b2e2e67
DerevoXP <derevoxp2015@gmail.com>
2020-08-09 15:48:10 +0400

    Добавил Васильева dvindex.php

---
+++ dvindex.php
@@ -0,0 +1,9 @@
+<?php
+
+echo("<h1>Hello! I am DerevoXP</h1>");
+echo("It is version 2");
+echo("Smth added to master");
+echo("It is version 4");
+echo("Experimental Developer!!!");
+
+echo("More experiments!");
