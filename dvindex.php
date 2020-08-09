4b9788c00953ceca6423f2a1722d0e5e846e4b0c
DerevoXP <derevoxp2015@gmail.com>
2020-08-09 16:51:18 +0400

    TEST DerevoXP

--- dvindex.php
+++ dvindex.php
@@ -1,9 +1,3 @@
 <?php
 
-echo("<h1>Hello! I am DerevoXP</h1>");
-echo("It is version 2");
-echo("Smth added to master");
-echo("It is version 4");
-echo("Experimental Developer!!!");
-
-echo("More experiments!");
+echo("TEST DerevoXP");
