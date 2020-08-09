1a5c86fc1b80ea39cc08f1e41af97223aa7b3b3d
yand <yand@outlook.com>
2020-08-09 18:41:18 +0300

    Test2

---
+++ app/Http/Controllers/YAController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class YAController
+{
+    public function hello() {
+        return view ("YA/hello");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,27 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/nz/hello', function () {
-    return "Hello, I am NZ!";
-});
-
-Route::get("/ya/hello", function(){
-    return "Hello, I am YA!";
-});
-Route::get("/vs/hello", function (){
-    return 'Hello, Vladimir';
-});
-Route::get('/dk/hello', function () {
-    return "HEllO, I AM DINAR!";
-});
-
-Route::get('/derevoxp', function () {
-    echo("Hello, DerevoXP!");
-});
-
-Route::get("ah/hello", function (){
-    return "Hello, I am AH!";
+    return view('welcome ');
 });
