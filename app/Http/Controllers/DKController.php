840d352580d5df6432d58ce24c1795e403577778
Dinar Khalfin <dinarhalfin@yahoo.com>
2020-08-09 18:02:55 +0300

    updated controller

--- app/Http/Controllers/DKController.php
+++ app/Http/Controllers/DKController.php
@@ -2,12 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
 
 class DKController extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
 }
