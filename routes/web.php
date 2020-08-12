99d250faa2af477b17507f546e605bba5c759a5e
Nikolay Zhukovets <7nz.zn7@gmail.com>
2020-08-12 21:59:29 +0300

    update view

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::get('/nz/hello', function () {
 
 Route::get('/nz/test/{word}', 'NZController@test');
 Route::get('/nz/events', 'NZController@events');
+Route::get('/nz/events/desc', 'NZController@eventsDesc');
+Route::get('/nz/events/asc', 'NZController@eventsAsc');
 
 Route::get("/ya/hello", function(){
     return "Hello, I am YA!";
