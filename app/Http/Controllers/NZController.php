6bf999fadf391b2f6c6d24b78b03ce5234e2a3bc
Nikolay Zhukovets <7nz.zn7@gmail.com>
2020-08-12 20:42:00 +0300

    add test

--- app/Http/Controllers/NZController.php
+++ app/Http/Controllers/NZController.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers;
 
 
 
+use App\NZEvent;
+
 class NZController
 {
     public function test($word) {
-
+        $events = NZEvent::select('name')->get();
+        echo '<pre>';
+        var_dump($events);
         return view('NZ/test', ["word"=>$word]);
     }
 }
