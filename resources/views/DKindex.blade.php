5b91252d9f2a718854f77d2499165cbae34e7297
Dinar Khalfin <dinarhalfin@yahoo.com>
2020-08-15 22:12:04 +0300

    update of DKindex

--- resources/views/DKindex.blade.php
+++ resources/views/DKindex.blade.php
@@ -18,29 +18,22 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <a class="navbar-brand" >Планировщик встреч</a>
+            <a class="navbar-brand" href="http://internet-shop.tmweb.ru">Планировщик мероприятий</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="http://internet-shop.tmweb.ru">Все записи</a></li>
-                <li ><a href="http://internet-shop.tmweb.ru/categories">Категории</a>
+                <li ><a >Все мероприятия</a></li>
+                <li ><a >Категории</a>
                 </li>
-                <li ><a href="http://internet-shop.tmweb.ru/basket">В корзину</a></li>
-                <li><a href="http://internet-shop.tmweb.ru/reset">Сбросить проект в начальное состояние</a></li>
-                <li><a href="http://internet-shop.tmweb.ru/locale/en">en</a></li>
+                <li ><a >Мои мероприятия</a></li>
+                <li><a >Сбросить проект в начальное состояние</a></li>
+                <li><a >en</a></li>
 
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">₽<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="http://internet-shop.tmweb.ru/currency/RUB">₽</a></li>
-                        <li><a href="http://internet-shop.tmweb.ru/currency/USD">$</a></li>
-                        <li><a href="http://internet-shop.tmweb.ru/currency/EUR">€</a></li>
-                    </ul>
-                </li>
+
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="http://internet-shop.tmweb.ru/login">Войти</a></li>
+                <li><a >Войти</a></li>
 
             </ul>
         </div>
@@ -50,7 +43,7 @@
 <div class="container">
     <div class="starter-template">
         <h1>Все товары</h1>
-        <form method="GET" action="http://internet-shop.tmweb.ru">
+        <form method="GET" action="">
             <div class="filters row">
                 <div class="col-sm-6 col-md-3">
                     <label for="price_from">Время от                    <input type="text" name="price_from" id="price_from" size="6" value="">
@@ -58,152 +51,9 @@
                     <label for="price_to">до                    <input type="text" name="price_to" id="price_to" size="6"  value="">
                     </label>
                 </div>
-                <div class="col-sm-2 col-md-2">
-                    <label for="hit">
-                        <input type="checkbox" name="hit" id="hit" > Хит                </label>
-                </div>
-                <div class="col-sm-2 col-md-2">
-                    <label for="new">
-                        <input type="checkbox" name="new" id="new" > Новинка                </label>
-                </div>
-                <div class="col-sm-2 col-md-2">
-                    <label for="recommend">
-                        <input type="checkbox" name="recommend" id="recommend" > Рекомендуем                </label>
-                </div>
-                <div class="col-sm-6 col-md-3">
-                    <button type="submit" class="btn btn-primary">Фильтр</button>
-                    <a href="http://internet-shop.tmweb.ru" class="btn btn-warning">Сброс</a>
-                </div>
             </div>
         </form>
-        <div class="row">
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <div class="labels">
 
-
-                    </div>
-                    <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg" alt="iPhone X 64GB">
-                    <div class="caption">
-                        <h3>iPhone X 64GB</h3>
-                        <p>71990 ₽</p>
-                        <p>
-                        <form action="http://internet-shop.tmweb.ru/basket/add/1" method="POST">
-                            <button type="submit" class="btn btn-primary" role="button">В корзину</button>
-                            <a href="http://internet-shop.tmweb.ru/mobiles/iphone_x_64"
-                               class="btn btn-default"
-                               role="button">Подробнее</a>
-                            <input type="hidden" name="_token" value="c8B2E444liOd13nrLklbOfo5e41RpiWMEua0sok4">            </form>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <div class="labels">
-
-
-                    </div>
-                    <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x_silver.jpg" alt="iPhone X 256GB">
-                    <div class="caption">
-                        <h3>iPhone X 256GB</h3>
-                        <p>89990 ₽</p>
-                        <p>
-                        <form action="http://internet-shop.tmweb.ru/basket/add/2" method="POST">
-                            <button type="submit" class="btn btn-primary" role="button">В корзину</button>
-                            <a href="http://internet-shop.tmweb.ru/mobiles/iphone_x_256"
-                               class="btn btn-default"
-                               role="button">Подробнее</a>
-                            <input type="hidden" name="_token" value="c8B2E444liOd13nrLklbOfo5e41RpiWMEua0sok4">            </form>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <div class="labels">
-
-
-                    </div>
-                    <img src="http://internet-shop.tmweb.ru/storage/products/htc_one_s.png" alt="HTC One S">
-                    <div class="caption">
-                        <h3>HTC One S</h3>
-                        <p>12490 ₽</p>
-                        <p>
-                        <form action="http://internet-shop.tmweb.ru/basket/add/3" method="POST">
-                            <button type="submit" class="btn btn-primary" role="button">В корзину</button>
-                            <a href="http://internet-shop.tmweb.ru/mobiles/htc_one_s"
-                               class="btn btn-default"
-                               role="button">Подробнее</a>
-                            <input type="hidden" name="_token" value="c8B2E444liOd13nrLklbOfo5e41RpiWMEua0sok4">            </form>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <div class="labels">
-
-
-                    </div>
-                    <img src="http://internet-shop.tmweb.ru/storage/products/iphone_5.jpg" alt="iPhone 5SE">
-                    <div class="caption">
-                        <h3>iPhone 5SE</h3>
-                        <p>17221 ₽</p>
-                        <p>
-                        <form action="http://internet-shop.tmweb.ru/basket/add/4" method="POST">
-                            <button type="submit" class="btn btn-primary" role="button">В корзину</button>
-                            <a href="http://internet-shop.tmweb.ru/mobiles/iphone_5se"
-                               class="btn btn-default"
-                               role="button">Подробнее</a>
-                            <input type="hidden" name="_token" value="c8B2E444liOd13nrLklbOfo5e41RpiWMEua0sok4">            </form>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <div class="labels">
-
-
-                    </div>
-                    <img src="http://internet-shop.tmweb.ru/storage/products/beats.jpg" alt="Наушники Beats Audio">
-                    <div class="caption">
-                        <h3>Наушники Beats Audio</h3>
-                        <p>20221 ₽</p>
-                        <p>
-                        <form action="http://internet-shop.tmweb.ru/basket/add/5" method="POST">
-                            <button type="submit" class="btn btn-primary" role="button">В корзину</button>
-                            <a href="http://internet-shop.tmweb.ru/portable/beats_audio"
-                               class="btn btn-default"
-                               role="button">Подробнее</a>
-                            <input type="hidden" name="_token" value="c8B2E444liOd13nrLklbOfo5e41RpiWMEua0sok4">            </form>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <div class="labels">
-
-
-                    </div>
-                    <img src="http://internet-shop.tmweb.ru/storage/products/gopro.jpg" alt="Камера GoPro">
-                    <div class="caption">
-                        <h3>Камера GoPro</h3>
-                        <p>12000 ₽</p>
-                        <p>
-                        <form action="http://internet-shop.tmweb.ru/basket/add/6" method="POST">
-                            <button type="submit" class="btn btn-primary" role="button">В корзину</button>
-                            <a href="http://internet-shop.tmweb.ru/portable/gopro"
-                               class="btn btn-default"
-                               role="button">Подробнее</a>
-                            <input type="hidden" name="_token" value="c8B2E444liOd13nrLklbOfo5e41RpiWMEua0sok4">            </form>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
         <nav>
             <ul class="pagination">
 
