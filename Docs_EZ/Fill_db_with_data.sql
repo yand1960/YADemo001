2f2f6c726a3a8b39e8bfd199926ddb4c52b106f4
accmailer <eugeny.sattler@gmail.com>
2020-08-16 18:48:53 +0300

    EZ commit SQL fill data script

---
+++ Docs_EZ/Fill_db_with_data.sql
@@ -0,0 +1,51 @@
+INSERT INTO `mydb`.`tblappointments`
+(`id_Disc`,
+`DisciplineName`,
+`DEscription`)
+VALUES
+(1,
+"Algebra",
+"Algebra for Beginners");
+
+INSERT INTO `mydb`.`tblappointments`
+(`id_Disc`,
+`DisciplineName`,
+`DEscription`)
+VALUES
+(2,
+"Algebra2",
+"Advanced Algebra");
+
+INSERT INTO `mydb`.`tblappointments`
+(`id_Disc`,
+`DisciplineName`,
+`DEscription`)
+VALUES
+(3,
+"Poetry1",
+"Russian poetry 6+");
+
+INSERT INTO `mydb`.`tblappointments`
+(`id_Disc`,
+`DisciplineName`,
+`DEscription`)
+VALUES
+(4,
+"Poetry2",
+"Russian poetry 18+");
+
+INSERT INTO `mydb`.`tblappointments`
+(`id_Disc`,
+`DisciplineName`,
+`DEscription`)
+VALUES
+(5,
+"Music",
+"Music for Beginners");
+
+INSERT INTO `mydb`.`tblevents` (`id_Exam`, `isApproved`, `TimeandDate`, `StudentName`, `tblEvents_id_Exam`, `tblAppointments_id_Disc`) VALUES ('2', '1', '2020-08-26', 'Ivanov Petr', '2', '2');
+INSERT INTO `mydb`.`tblevents` (`id_Exam`, `isApproved`, `TimeandDate`, `StudentName`, `tblEvents_id_Exam`, `tblAppointments_id_Disc`) VALUES ('3', '1', '2020-08-27', 'Ivanov Petr', '3', '3');
+INSERT INTO `mydb`.`tblevents` (`id_Exam`, `isApproved`, `TimeandDate`, `StudentName`, `tblEvents_id_Exam`, `tblAppointments_id_Disc`) VALUES ('4', '1', '2020-08-28', 'Ivanov Petr', '4', '4');
+INSERT INTO `mydb`.`tblevents` (`id_Exam`, `isApproved`, `TimeandDate`, `StudentName`, `tblEvents_id_Exam`, `tblAppointments_id_Disc`) VALUES ('5', '1', '2020-08-29', 'Ivanov Petr', '5', '5');
+
+SELECT * FROM mydb.tblappointments;
