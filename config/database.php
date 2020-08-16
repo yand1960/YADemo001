3cb1518eb8fed86a0191969236a369765bc78935
yand <yand@outlook.com>
2020-08-17 00:26:09 +0300

    DB User changed

--- config/database.php
+++ config/database.php
@@ -35,33 +35,7 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
+        
 
         'mysqlVS' => [
             'driver' => 'mysql',
@@ -69,7 +43,7 @@ return [
             'host' => 'mama',
             'port' =>  '3306',
             'database' => 'mydbVS',
-            'username' => 'root',
+            'username' => 'exams',
             'password' => 'Pa$$w0rd',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -104,7 +78,7 @@ return [
             'host' => 'mama',
             'port' => '3306',
             'database' => 'dkappointments',
-            'username' => 'root',
+            'username' => 'exams',
             'password' => 'Pa$$w0rd',
             'charset' => 'utf8',
             'prefix' => '',
@@ -116,40 +90,30 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-        ],
-
         'nz_mysql' => [
             'driver' => 'mysql',
             'host' => 'mama',
             'port' => '3306',
             'database' => 'nzappointments',
-            'username' => 'root',
+            'username' => 'exams',
+            'password' => 'Pa$$w0rd',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+		
+		'ah_mysql' => [
+            'driver' => 'mysql',
+            'host' => 'mama',
+            'port' => '3306',
+            'database' => 'ah',
+            'username' => 'exams',
             'password' => 'Pa$$w0rd',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
