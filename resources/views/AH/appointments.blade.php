ec95ed5eda178de1eb339c0ee1a7a58dc282eb56
cthegost <cthegost@gmail.com>
2020-08-23 18:09:02 +0300

    починил дату, теперь все работает

--- resources/views/AH/appointments.blade.php
+++ resources/views/AH/appointments.blade.php
@@ -31,7 +31,7 @@
         <th scope="col">Exam time</th>
         <th scope="col" >
             <div style="display: inline-block">Appointments</div>
-            <div style="display: inline-block; position: absolute; right: 1%"><a href="{{ action('AHController@examList') }}" class="badge badge-light">Return to the list of exams</a></div>
+            <div style="display: inline-block; position: absolute; right: 1%"><a href="{{ action('AHController@events') }}" class="badge badge-light">Return to the list of exams</a></div>
         </th>
     </tr>
     </thead>
@@ -43,7 +43,8 @@
                 <td>
                     <form method="get" action="{{ action('AHController@makeAppointment') }}">
                         <div class="form-group">
-                            <input type="text" size="1" name="nameInput" required class="form-control-inline" style="width: 50%" id="exampleFormControlInput1" placeholder="Enter your name">
+                            <input type="text" size="1" name="nameInput" required class="form-control-inline" style="
+                            width: 50%" id="exampleFormControlInput1" placeholder="Enter your name">
                             <input type="hidden" value="{{$d['Id']}}" name="appointmentId"/>
                             <button type="submit" class="btn btn-primary btn-sm">Make an appointment</button>
                         </div>
